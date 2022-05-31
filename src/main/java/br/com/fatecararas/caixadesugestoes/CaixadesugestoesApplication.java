c106d84f824a31f0586547a7934fe9f4dc78ebaf
Esdras B Silva <bzr4@icloud.com>
2022-05-31 19:44:39 -0300

    master
    - Criação de arquivos .properties.

---
+++ src/main/java/br/com/fatecararas/caixadesugestoes/CaixadesugestoesApplication.java
@@ -0,0 +1,13 @@
+package br.com.fatecararas.caixadesugestoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaixadesugestoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaixadesugestoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fatecararas/caixadesugestoes/CaixadesugestoesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fatecararas.caixadesugestoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaixadesugestoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
